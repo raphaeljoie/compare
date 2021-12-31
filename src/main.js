d1b16bf86d0b9fffe2640f3868e17813217a4e85
raphaeljoie <9681129+raphaeljoie@users.noreply.github.com>
2021-12-31 21:08:20 +0100

    add support for NaN

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 export function compare(x, y) {
-  return x === y;
+  if (x === y) return true;
+
+  if (Number.isNaN(x) && Number.isNaN(y) && typeof x === 'number' && typeof y === 'number') {
+    return true;
+  }
+
+  return false;
 }
--- test/test.js
+++ test/test.js
@@ -5,7 +5,34 @@ describe('compare()', () => {
   it('should return true for same integers', () => {
     assert.isOk(compare(1, 1));
   });
+  it('should return false for integer and same integer stringified', () => {
+    assert.isNotOk(compare(1, "1"));
+  });
+  it('should return true for same boolean', () => {
+    assert.isOk(compare(!0, !0));
+  });
+  it('should return true for both undefined', () => {
+    assert.isOk(compare(void 0, void 0));
+  });
+  it('should return true for both null', () => {
+    assert.isOk(compare(null, null));
+  });
+  it('should return false for null and undefined', () => {
+    assert.isNotOk(compare(null, void 0));
+  });
   it('should return false for non equal integers', () => {
     assert.isNotOk(compare(2, 1));
+  });
+  it('should return true for NaN comparison', () => {
+    assert.isOk(compare(parseInt('blabetiblou'), NaN));
+  });
+  it('should return true for same strings', () => {
+    assert.isOk(compare('blabetiblou', 'blabetiblou'));
+  });
+  it('should return false for distinct strings', () => {
+    assert.isNotOk(compare('blabetiblou', 'blabetiblou2'));
+  });
+  it('should return true for int and float same value', () => {
+    assert.isOk(compare(1, 1.0));
   });
 });
